3ffea803b06e8d683fb5c5e483cd86da9c653c9e
Woloyor <yuralysyshak@gmail.com>
2023-02-04 13:51:29 +0200

    OpenAI

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -27,7 +27,7 @@ class _ChatScreenState extends State<ChatScreen> {
   void initState() {
     super.initState();
     chatGPT = ChatGPT.instance.builder(
-      "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",
+      "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",
     );
   }
 
